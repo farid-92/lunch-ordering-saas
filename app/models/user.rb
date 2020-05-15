0f712dbc3b92b733fa8f90b255169b2689f27dc1
Farid <farid.babazov@gmail.com>
2020-05-16 00:07:06 +0600

    Add one-to-many relationship beetween User and Organization model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :organization, optional: true
+
+  before_create :organization_admin_exists?
+
+  enum role: [:user, :admin]
+
+  def organization_admin_exists?
+    organization = self.organization
+    self.role = User.where(organization_id: organization.id ).size == 0 ? 1 : 0
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_131826) do
+ActiveRecord::Schema.define(version: 2020_05_15_172525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,12 @@ ActiveRecord::Schema.define(version: 2020_05_15_131826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", default: ""
+    t.bigint "organization_id"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "organizations"
 end
